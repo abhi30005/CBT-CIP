8817ce6e7ab1cdd355393b97fa790a297f4181de
Abhijit Bhunia <bhuniaabhijit721644@gmail.com>
2024-09-15 20:40:16 +0530

    Update Student_Grade_Cal.c

--- Student_Grade_Cal.c
+++ Student_Grade_Cal.c
@@ -4,64 +4,66 @@
 #include <string.h>
 #define max 50
 
-typedef struct {
+typedef struct{
     char name[max],n1[max];
     char subjects[max][max];
     float scores[max];
     int num_subjects;
-} stud;
+}stud;
 
 void choose_subject(stud *stud,int i){
     int ch;
-		system("cls");
-        printf("\n1.Mathematics\n2.Chemistry\n3.Physics\n4.Biology\n5.English\n6.Bengali\n7.Others.\n");
-        printf("Choose subject %d: ",i+1);
-		scanf("%d",&ch);
-        switch(ch){
-            case 1:	
-                strcpy(stud->subjects[i],"Mathematics");
-                break;
-            case 2:	
-                strcpy(stud->subjects[i],"Chemistry");
-                break;
-            case 3:	
-                strcpy(stud->subjects[i],"Physics");
-                break;
-            case 4:	
-                strcpy(stud->subjects[i],"Biology");
-                break;
-            case 5:	
-                strcpy(stud->subjects[i],"English");
-                break;
-            case 6:	
-                strcpy(stud->subjects[i],"Bengali");
-                break;
-            case 7:
-                printf("\nEnter your Subject:");
-                scanf("%s",stud->subjects[i]);
-                break;
-        }
-    }	
+	system("cls");
+    printf("\n1.Mathematics\n2.Chemistry\n3.Physics\n4.Biology\n5.English\n6.Bengali\n7.Others.\n");
+    printf("Choose subject %d: ",i+1);
+	scanf("%d",&ch);
+    switch(ch){
+        case 1:	
+            strcpy(stud->subjects[i],"Mathematics");
+            break;
+        case 2:	
+            strcpy(stud->subjects[i],"Chemistry");
+            break;
+        case 3:	
+            strcpy(stud->subjects[i],"Physics");
+            break;
+        case 4:	
+            strcpy(stud->subjects[i],"Biology");
+            break;
+        case 5:	
+            strcpy(stud->subjects[i],"English");
+            break;
+        case 6:	
+            strcpy(stud->subjects[i],"Bengali");
+            break;
+        case 7:
+            printf("\nEnter your Subject:");
+            scanf("%s",stud->subjects[i]);
+            break;
+    }
+}	
 
 
-void add_score(stud* stud, char* subject, float score) {
-    for (int i = 0; i < stud->num_subjects; i++) {
-        if (strcmp(stud->subjects[i], subject) == 0) {
-            stud->scores[i] = score;
+void add_score(stud* stud,char* subject,float score){
+	int i;
+    for (i=0;i<stud->num_subjects;i++) {
+        if (strcmp(stud->subjects[i],subject)==0){
+            stud->scores[i]=score;
             return;
         }
     }
     strcpy(stud->subjects[stud->num_subjects], subject);
-    stud->scores[stud->num_subjects] = score;
+    stud->scores[stud->num_subjects]=score;
     stud->num_subjects++;
 }
 
 float calculate_average(stud* stud) {
-    float sum = 0,avg=0;
-    for (int i = 0; i < stud->num_subjects; i++) {
-        sum += stud->scores[i];
+    float sum=0,avg=0;
+    int i;
+    for(i=0;i<stud->num_subjects;i++){
+        sum+=stud->scores[i];
     }
-    avg=sum / stud->num_subjects;
+    avg=sum/stud->num_subjects;
     if(avg>=90.0)
     	printf("\nGrade is: O");
     else if(80<=avg<90)
@@ -74,36 +76,36 @@ float calculate_average(stud* stud) {
 		printf("\nGrade is: C"); 
 	else
 		printf("\nGrade is: Fail"); 
-return avg;
+	return avg;
 }
 
-char* get_highest_score(stud* stud) {
-    int max_index = 0;
-    for (int i = 1; i < stud->num_subjects; i++) {
-        if (stud->scores[i] > stud->scores[max_index]) {
-            max_index = i;
+int get_highest_score_index(stud* stud){
+    int max_index=0,i;
+    for(i=0;i<stud->num_subjects;i++){
+        if(stud->scores[i]>stud->scores[max_index]){
+            max_index=i;
         }
     }
-    return stud->subjects[max_index];
+    return max_index;
 }
 
-char* get_lowest_score(stud* stud) {
-    int min_index = 0;
-    for (int i = 1; i < stud->num_subjects; i++) {
-        if (stud->scores[i] < stud->scores[min_index]) {
-            min_index = i;
+int get_lowest_score_index(stud* stud) {
+    int min_index=0,i;
+    for(i=0;i<stud->num_subjects;i++) {
+        if(stud->scores[i]<stud->scores[min_index]) {
+            min_index=i;
         }
     }
-    return stud->subjects[min_index];
+    return min_index;
 }
 
 void display_performance(stud* stud){
-    char* highest_score = get_highest_score(stud);
-    char* lowest_score = get_lowest_score(stud);
+    int max_index=get_highest_score_index(stud);
+    int min_index=get_lowest_score_index(stud);
     printf("Student: %s %s\n",strupr(stud->name),strupr(stud->n1));
     printf("\nAverage Score: %.2f\n",calculate_average(stud));
-    printf("Highest Score --> %s : %.2f\n", highest_score, stud->scores[stud->num_subjects - 1]);
-    printf("Lowest Score --> %s : %.2f\n", lowest_score, stud->scores[0]);
+    printf("Highest Score --> %s : %.2f\n",stud->subjects[max_index],stud->scores[max_index]);
+    printf("Lowest Score --> %s : %.2f\n",stud->subjects[min_index],stud->scores[min_index]);
     printf("\n");
 }
 
@@ -111,7 +113,7 @@ int main() {
     stud studs[max];
     int num_studs = 0;
     while (1) {
-        printf("1. Add student.\n");
+        printf("1. Add Student.\n");
         printf("2. Display student Performance.\n");
         printf("3. Exit.\n");
         printf("\n\nEnter Your Choice:");
@@ -120,19 +122,19 @@ int main() {
         system("cls");
         switch(choice){
          case 1:
-            printf("Enter student name: ");
+            printf("Enter Student Name: ");
             scanf("%s %s",studs[num_studs].name,studs[num_studs].n1);
-            printf("\nEnter number of subjects: ");
+            printf("\nEnter Number of Subjects: ");
             scanf("%d",&studs[num_studs].num_subjects);
-            for (i=0;i<studs[num_studs].num_subjects;i++) {
+            for (i=0;i<studs[num_studs].num_subjects;i++){
                 choose_subject(&studs[num_studs],i);
                 printf("Enter score for %s: ",studs[num_studs].subjects[i]);
-                scanf("%f", &studs[num_studs].scores[i]);
+                scanf("%f",&studs[num_studs].scores[i]);
             }
             num_studs++;
             break;
         case 2:
-            for (int i = 0; i < num_studs; i++) {
+            for(i=0;i<num_studs;i++){
                 display_performance(&studs[i]);
             }
             break; 
